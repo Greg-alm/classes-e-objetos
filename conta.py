0abe72c414192143af0e112ac222bd44b8d2a470
Greg-alm <a.gregory@escola.pr.gov.br>
2026-05-29 11:22:16 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,6 +1,45 @@
-class Conta:
+class  Conta:
     def __init__(self, numero, titular, saldo, limite):
-      self.numero = numero
-      self.titular = titular
-      self.saldo = saldo
-      self.limite = limite
+       self.__numero = numero
+       self.__titular = titular
+       self.__saldo = saldo
+       self.__limite = limite
+
+    #Declaração dos métodos (função)
+    def extrato(self):
+        print(f"Saldo {self.__saldo} do titular {self.__titular}")
+
+    def depositar(self, valor):
+        self.__saldo += valor
+    
+    def sacar(self, valor):
+        if(self.__saldo < valor):
+            print(f"Não é possível sacar o valor desejado")
+        
+        else:
+            self.__saldo -= valor
+    
+    def transferir(self, valor, destino):
+        if (self.__saldo < valor) or (valor < 0):
+            print("Saldo insuficiente")
+        else:
+            self.sacar(valor)
+            destino.depositar(valor)
+    #metodos para retornar apenas valores das prorpiedades
+    @property
+    def saldo(self):
+        return self.__saldo 
+    @property
+    def titular(self):
+        return self.__titular
+    @property
+    def numero(self):
+        return self.__numero
+    @property
+    def limite(self):
+        return self.__limite
+    
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
